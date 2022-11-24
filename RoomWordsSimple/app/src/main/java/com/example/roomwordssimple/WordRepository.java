8900132e3009a93f5cde4290e43f8f0e263f3624
POST2 <post2.prog2@enetcom.usf.tn>
2022-11-24 17:01:43 +0100

    first commit

---
+++ RoomWordsSimple/app/src/main/java/com/example/roomwordssimple/WordRepository.java
@@ -0,0 +1,46 @@
+package com.example.roomwordssimple;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application) {
+        WordRoomDataBase db = WordRoomDataBase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+
+        public void execute(Word word) {
+        }
+    }
+}
